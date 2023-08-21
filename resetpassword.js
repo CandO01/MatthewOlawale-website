71fd0e229b58eb6856b7d186ff1d6fdcc91fad48
CandO01 <119475643+CandO01@users.noreply.github.com>
2023-08-22 00:07:26 +0100

    Add files via upload

---
+++ resetpassword.js
@@ -0,0 +1,9 @@
+
+function resetPassword(){
+    const inBox =  document.querySelector('.reset');
+    if(inBox.innerHTML === 'Reset'){
+        inBox.innerHTML = alert('The link to reset your password has been sent successfully to you email box!!!');
+    };
+    
+     
+};
